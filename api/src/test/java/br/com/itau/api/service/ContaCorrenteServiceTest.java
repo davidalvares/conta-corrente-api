415e25972685691e81b3be53a9a7d9847cecb4fa
David Alvares <davidanalista007@gmail.com>
2021-03-18 15:57:45 -0300

    [feature/conta_corrente]: implementação do testes unitários .

--- api/src/test/java/br/com/itau/api/service/ContaCorrenteServiceTest.java
+++ api/src/test/java/br/com/itau/api/service/ContaCorrenteServiceTest.java
@@ -1,12 +1,15 @@
 package br.com.itau.api.service;
 
+import br.com.itau.api.exception.ObjectNotFoundException;
 import br.com.itau.api.model.ContaCorrente;
 import br.com.itau.api.model.ModelsBuilder;
 import br.com.itau.api.repository.ContaCorrenteRepository;
 import br.com.itau.api.service.impl.ContaCorrenteServiceImpl;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
@@ -27,5 +30,39 @@ public class ContaCorrenteServiceTest {
         doReturn(contaCorrente).when(repository).save(any(ContaCorrente.class));
         service.save(contaCorrente);
         verify(repository,times(1)).save(any(ContaCorrente.class));
+    }
+
+    @Test
+    public void findByIdTest(){
+        ContaCorrente contaCorrente = ModelsBuilder.contaCorrente();
+        doReturn(Optional.ofNullable(contaCorrente)).when(repository).findById(anyLong());
+        service.findById(contaCorrente.getId());
+        verify(repository,times(1)).findById(anyLong());
+    }
+
+    @Test
+    public void upateTest(){
+        ContaCorrente contaCorrente = ModelsBuilder.contaCorrente();
+        doReturn(Optional.ofNullable(contaCorrente)).when(repository).findById(anyLong());
+        doReturn(contaCorrente).when(repository).save(any(ContaCorrente.class));
+        service.update(contaCorrente.getId(),contaCorrente);
+        verify(repository,times(1)).findById(anyLong());
+        verify(repository,times(1)).save(any(ContaCorrente.class));
+    }
+
+    @Test
+    public void upateObjectNotFoundExceptionTest(){
+        ContaCorrente contaCorrente = ModelsBuilder.contaCorrente();
+        doReturn(Optional.empty()).when(repository).findById(anyLong());
+        Assertions.assertThrows(ObjectNotFoundException.class, ()->service.update(contaCorrente.getId(),contaCorrente));
+        verify(repository,times(1)).findById(anyLong());
+    }
+
+    @Test
+    public void findByCpfCnpjTest(){
+        ContaCorrente contaCorrente = ModelsBuilder.contaCorrente();
+        doReturn(Optional.ofNullable(contaCorrente)).when(repository).findByCpfCnpj(anyString());
+        service.findByCpfCnpj(contaCorrente.getCpfCnpj());
+        verify(repository,times(1)).findByCpfCnpj(anyString());
     }
 }
